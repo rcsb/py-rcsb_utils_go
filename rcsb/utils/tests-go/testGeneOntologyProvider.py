4b9d9116a0418c17be63214470d40301d5dee24b
Dennis Piehl <dennis.piehl@rcsb.org>
2025-07-29 13:55:52 -0400

    V0.21 Update tests based on updated source GO data

--- rcsb/utils/tests-go/testGeneOntologyProvider.py
+++ rcsb/utils/tests-go/testGeneOntologyProvider.py
@@ -1,11 +1,11 @@
 ##
-# File:    GeneOntologyProviderTests.py
+# File:    testGeneOntologyProvider.py
 # Author:  J. Westbrook
 # Date:    10-Dec-2019
 # Version: 0.001
 #
-# Update:
-#
+# Updates:
+#   29-Jul-2025 dwp Update tests based on changes to GO data on 2025-07-22
 #
 ##
 """
@@ -41,7 +41,7 @@ class GeneOntologyProviderTests(unittest.TestCase):
     def testReloadGeneOntology(self):
         """Test load from source"""
         try:
-            goP = GeneOntologyProvider(goDirPath=self.__workPath, useCache=True)
+            goP = GeneOntologyProvider(goDirPath=self.__workPath, useCache=False)
             ok = goP.testCache()
             self.assertTrue(ok)
             #
@@ -61,8 +61,9 @@ class GeneOntologyProviderTests(unittest.TestCase):
             # These numbers may change as some terms become obsolete
             # in future GO updates.
             # See http://geneontology.org/stats.html and https://github.com/geneontology/go-announcements
-            goIdL = [("GO:0023052", 1), ("GO:0051179", 1), ("GO:0070727", 4), ("GO:1990747", 28)]
+            goIdL = [("GO:0023052", 1), ("GO:0051179", 1), ("GO:0033036", 2), ("GO:1990747", 26)]
             for goId, numParents in goIdL:
+                print(goId)
                 nm = goP.getName(goId)
                 self.assertIsNotNone(nm)
                 nL = goP.getAdjacentParents(goId)
@@ -73,14 +74,14 @@ class GeneOntologyProviderTests(unittest.TestCase):
                 self.assertIsNotNone(nL)
                 linL = goP.getDescendants(goId, includeSelf=False)
                 self.assertGreaterEqual(len(linL), numParents)
-                logger.debug("%a Lineage(%d) %r", goId, len(linL), linL)
+                logger.info("%a Lineage(%d) %r", goId, len(linL), linL)
 
             # Test getting tree node list with input GO ID list
             gIdL = [tup[0] for tup in goIdL]
             trL = goP.exportTreeNodeList(filterL=gIdL)
             logger.debug("trL %r", trL)
             logger.info("Length of filtered tree node list %d", len(trL))
-            self.assertGreaterEqual(len(trL), 30)
+            self.assertGreaterEqual(len(trL), 28)
 
             # Test getting full tree node list (without input GO ID list)
             gIdL = [tup[0] for tup in goIdL]
